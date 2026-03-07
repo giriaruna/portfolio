e79f63383b810a1cb9a8455c1584dca6d649931b
giriaruna <ag8876@nyu.edu>
2026-03-06 23:31:55 -0500

    Add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+// the Analytics component
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      {/* to track your visitors */}
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
